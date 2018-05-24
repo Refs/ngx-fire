7fe86b319e60a4609d193e1886fa8c1877bfeaa3
Refs <lhx1010339808@outlook.com>
2018-05-24 16:51:55 +0800

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { StoreModule } from '@ngrx/store';
 import { postReducer } from './state/posts/post.reducer';
@@ -21,6 +21,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     StoreModule.forRoot({
       post: postReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
     })
   ],
   providers: [],
